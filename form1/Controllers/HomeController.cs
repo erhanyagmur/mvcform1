912440f29aaafc00e41cc7aaaa587e7b0767ff00
erhanyagmur <119954665+erhanyagmur@users.noreply.github.com>
2023-05-15 21:05:51 +0300

    Add files via upload

---
+++ form1/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using form1.Models;
+
+namespace form1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+            public IActionResult VeriAlGet(int okulno,string ad,string soyad,string sinif)
+            {
+            return Content(okulno+"-"+ad+"-"+soyad+"-"+sinif);
+            }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
